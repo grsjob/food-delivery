2746fc7d4ce91a81c647814ce809bcd28368a8be
grsjob <grsgithub@gmail.com>
2021-11-05 23:56:29 +0300

    сделал обязательное задание

--- js/main.js
+++ js/main.js
@@ -2,8 +2,30 @@ const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
 const logInForm = document.getElementById('logInForm');
-const login = document.getElementById('login');
+const inputLogin = document.getElementById('login');
+const inputPassword = document.getElementById('password');
+const buttonOut = document.querySelector('.button-out');
+const userName = document.querySelector('.user-name');
 
+
+
+const login = (user) => {
+	buttonAuth.style.display = 'none';
+	buttonOut.style.display = 'flex';
+	userName.style.display = 'flex';
+	modalAuth.style.display = 'none';
+
+	userName.textContent = user.login;
+}
+const logout = (user) => {
+	buttonAuth.style.display = 'flex';
+	buttonOut.style.display = 'none';
+	userName.style.display = 'none';
+	modalAuth.style.display = 'none';
+	userName.textContent = '';
+	localStorage.removeItem('user');
+
+}
 
 buttonAuth.addEventListener('click', () => {
 	modalAuth.style.display = 'flex';
@@ -14,9 +36,20 @@ closeAuth.addEventListener('click', () => {
 
 logInForm.addEventListener('submit', (event) => {
 	event.preventDefault();
-	console.log('auth');
 
+	const user = {
+		login: inputLogin.value,
+		password: inputPassword.value
+	}
+	localStorage.setItem('user', JSON.stringify(user));
+	login(user);
 
 })
 
+buttonOut.addEventListener('click', () => {
+	logout();
+})
 
+if (localStorage.getItem('user')) {
+	login(JSON.parse(localStorage.getItem('user')));
+}
